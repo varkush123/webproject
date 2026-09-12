e1941101c872046bd2ebaf65656d88f5f6d3d8df
varkush123 <kushwahavarsha270@gmail.com>
2026-09-12 19:42:18 +0530

    codingthunder frontend

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/hello")
+def hello ():
+   return {"message":"hello world"}
+@app.route("/")
 def home ():
    return render_template('index.html')
 
